2ee039c58a05a2a443c4029c8bc47f6397610448
IgorD <igordolgushev@gmail.com>
2018-01-10 20:15:33 +0300

    Upload

---
+++ src/Game/Puck.java
@@ -0,0 +1,50 @@
+package Game;
+
+import java.io.IOException;
+
+/**
+ * Created by Игорь on 01.01.2018.
+ */
+class Puck {
+    private String name, suit;
+    private Integer value;
+    private boolean used;
+
+    public Puck(String Name,String Suite,Integer Value)
+    {
+        this.name = Name;
+        this.suit = Suite;
+        this.value = Value;
+        this.used = false;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getName(){return name;}
+
+    /**
+     *
+     * @return
+     */
+    public String getSuit(){return suit;}
+
+    /**
+     *
+     * @return
+     */
+    public Integer getValue(){return value;}
+
+    /**
+     *
+     * @return
+     */
+    public boolean getUsed(){return used;}
+
+    /**
+     *
+     */
+    public void setUsed(){this.used = true;}
+
+}
---
+++ src/Game/Statistic.java
@@ -0,0 +1,40 @@
+package Game;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Statistic {
+
+    /**
+     *
+     * @param puck
+     * @param value
+     * @return
+     * @throws IOException
+     */
+    public Integer HopeSuccessTake(ArrayList<Puck> puck, int value) throws IOException
+    {
+        int count= 0;
+
+        for (Puck pck : puck) {
+            if (pck.getValue() <= value) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     *
+     * @param hopeSuccessTake
+     * @param sizePuck
+     * @return
+     * @throws IOException
+     */
+    public Integer HopeSuccessTakePrc(int hopeSuccessTake, int sizePuck) throws IOException
+    {
+        return (hopeSuccessTake/sizePuck * 100);
+    }
+
+}
